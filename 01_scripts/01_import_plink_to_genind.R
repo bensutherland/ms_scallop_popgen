110ecae1710227da3e0350effe4fa4d082465853
GreenT <bensutherland7@gmail.com>
2023-03-21 11:24:50 -0700

    Rename plink to genind script

--- 01_scripts/01_import_plink_to_genind.R
+++ 01_scripts/01_import_plink_to_genind.R
@@ -1,4 +1,4 @@
-# Main popgen analysis script for Yesso scallop 
+# Import plink data and convert to genind
 # Initialized 2022-12-05, B. Sutherland
 
 #### Front Matter ####
@@ -13,7 +13,7 @@ library("rstudioapi")
 library("adegenet")
 
 
-# Set working directory
+# Set working directory to the ms_scallop_popgen repo
 current.path <- dirname(rstudioapi::getSourceEditorContext()$path)
 current.path <- gsub(pattern = "\\/ms_scallop_popgen\\/01_scripts", replacement = "", x = current.path)
 current.path <- paste0(current.path, "/stacks_workflow")
@@ -24,23 +24,12 @@ rm(current.path)
 # sessionInfo()
 
 # Set variables
-output.dir <- "12-results"
-input.FN <- "05-stacks/populations_single_snp.raw"
-
+input.FN <- "05-stacks/popn_out_single_snp/populations_single_snp.raw"
 
 #### 1. Import data ####
 print(paste0("Loading data from ", input.FN))
 my.data <- read.PLINK(file = input.FN)
 my.data
-
-# ## Explore data attributes
-# nInd(my.data) # How many individuals?
-# nLoc(my.data) # How many loci?
-# indNames(my.data)[1:20] # What individuals?
-# nPop(my.data) # How many pops?
-# unique(pop(my.data)) # What pops?
-# locNames(my.data, withAlleles = T)[1:20] # What allele names?
-
 
 # Update population names
 pop(my.data) <- gsub(pattern = 1, replacement = "BC", x = pop(my.data))
@@ -48,30 +37,7 @@ pop(my.data) <- gsub(pattern = 2, replacement = "VIU", x = pop(my.data))
 pop(my.data) <- gsub(pattern = 3, replacement = "JPN", x = pop(my.data))
 # unique(pop(my.data)) # What pops?
 
-
-#### 2. Data exploration (allele frequency) ####
-# Plot instances of minor allele across individuals and loci
-png(file = paste0(output.dir, "/", "glPlot.png"), width = 924, height = 600)
-glPlot(x = my.data, posi="topleft") 
-dev.off()
-
-# Create density plot of minor allele frequencies
-pdf(file = paste0(output.dir, "/", "maf_hist.pdf"), width = 6, height = 4)
-myFreq <- glMean(my.data)
-hist(myFreq
-     #, proba=T # note: does not sum to 1, not worth using
-     , col="gold", xlab = "Minor allele frequency (MAF)"
-     , main = ""
-     , ylim = c(0, 2500)
-     , ylab = "Number of loci"
-     , las = 1
-     , breaks = 20
-     )
-text(x = 0.4, y = 1500, labels = paste("n = ", nLoc(my.data), " loci", sep = "" ))
-dev.off()
-
-
-#### 3. Convert genlight to genind ####
+#### 2. Convert genlight to genind ####
 # Convert genlight to matrix
 my.data.mat <- as.matrix(my.data)
 my.data.mat[1:5,1:5]
@@ -92,4 +58,4 @@ unique(pop(my.data.gid))
 # Data is now a genind, and therefore can be used with simple_pop_stats
 save(my.data.gid, file="../ms_scallop_popgen/03_results/prepared_genind.RData")
 
-# Next go to "ms_scallop_popgen/01_scripts/pyes_popgen_simple_pop_stats.R"
+# Next go to "ms_scallop_popgen/01_scripts/02_sps_char_and_filt.R"
