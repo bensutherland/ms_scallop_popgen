326c3fda27420c8a6f065b26af8e69e015e43288
GreenT <bensutherland7@gmail.com>
2023-03-22 14:31:24 -0700

    Update script name and add MAF plotting and comparison

--- 01_scripts/03_sps_analysis.R
+++ 01_scripts/03_sps_analysis.R
@@ -1,13 +1,61 @@
 # simple_pop_stats analysis component of the Yesso scallop RADseq analysis
 # B. Sutherland
 # Initialized 2022-12-05
-# Requires running "ms_scallop_popgen/01_scripts/pyes_popgen_simple_pop_stats.R" first
+# Requires first running "ms_scallop_popgen/01_scripts/02_sps_char_and_filt.R"
 
 #### 01. Load Data ####
 load(file = "03_results/post_all_filters.RData") # loaded from prerequisite script above
 
 # Data is present in
 obj
+
+
+#### 02. Compare MAF ####
+# Create dataset per population
+obj.VIU <- obj.sep$VIU
+obj.JPN <- obj.sep$JPN
+
+maf_filt(data = obj.VIU, maf = 0.01)
+obj.VIU <- obj_maf_filt
+myFreq.VIU <- myFreq
+
+maf_filt(data = obj.JPN, maf = 0.01)
+obj.JPN <- obj_maf_filt
+myFreq.JPN <- myFreq
+
+# How many variants and what percentage are under 0.1? 
+table(myFreq.VIU < 0.1)
+table(myFreq.JPN < 0.1)
+
+table(myFreq.VIU < 0.1)[2] / length(myFreq.VIU) # 49.7%
+table(myFreq.JPN < 0.1)[2] / length(myFreq.JPN) # 61.8%
+
+# Plot
+pdf(file = paste0("03_results/MAF_hist_VIU_JPN.pdf"), width = 7, height = 4)
+par(mfrow=c(1,2))
+hist(myFreq.VIU
+     #, proba=T # note: does not sum to 1, not worth using
+     , col="grey", xlab = "MAF (VIU)"
+     , main = ""
+     , ylim = c(0, 1250)
+     , ylab = "Number of loci"
+     , las = 1
+     , breaks = 20
+)
+text(x = 0.4, y = 800, labels = paste("n = ", length(myFreq.VIU), " loci", sep = "" ))
+
+hist(myFreq.JPN
+     #, proba=T # note: does not sum to 1, not worth using
+     , col="grey", xlab = "MAF (JPN)"
+     , main = ""
+     , ylim = c(0, 1250)
+     , ylab = "Number of loci"
+     , las = 1
+     , breaks = 20
+)
+text(x = 0.4, y = 800, labels = paste("n = ", length(myFreq.JPN), " loci", sep = "" ))
+dev.off()
+
 
 #### 04. Analysis ####
 ## Multivariate
